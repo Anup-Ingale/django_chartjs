f726c1504467dc9bc89fbae88fba11b2115db84b
Anup-Ingale <42895688+Anup-Ingale@users.noreply.github.com>
2021-07-18 19:47:45 +0530

    final code

--- visualize/urls.py
+++ visualize/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', ChartView.as_view() , name ='visual'),
+    path('', chartview , name ='visual'),
 ]
--- visualize/views.py
+++ visualize/views.py
@@ -1,24 +1,20 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
+
 from .models import  *
 
 # Create your views here.
 
-class ChartView(APIView):
-    authentication_classes = []
-    permission_classes = []
-    def get(self, request, format=None):
-        query = Sales.objects.all()
-        labels = []
-        default_items = []
+def chartview(request):
+    query = Sales.objects.all()
+    labels = []
+    default_items = []
 
-        for item in query:
-            labels.append(item.name_of_sales)
-            default_items.append(item.profit)
+    for item in query:
+        labels.append(item.name_of_sales)
+        default_items.append(item.profit)
 
-        data = {
-            "labels": labels,
-            "default": default_items,
-        }
-        return Response(data)
+    data = {
+        "labels": labels,
+        "datasets": default_items,
+    }
+    return render(request,'visual.html',context=data)
